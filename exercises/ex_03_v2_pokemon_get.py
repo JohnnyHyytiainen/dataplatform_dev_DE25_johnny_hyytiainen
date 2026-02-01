ab982573a705b0f9704870a775453f544fe995b0
Johnny Hyytiäinen <Johnny.Hyytiainen@gmail.com>
2026-02-01 18:23:29 +0100

    feat(exercise) API + GET method exercises. Looping over a function to not hardcode pokemons to print

---
+++ exercises/ex_03_v2_pokemon_get.py
@@ -0,0 +1,32 @@
+import httpx
+from pydantic import BaseModel
+from exersice_schema.ex_03_pokemon_schema import PokemonSchema
+
+
+# Exercise 3 v2. Gå från hardcoded pokemon till lista jag enkelt kan fylla i med olika pokemon och iterera över
+# + mappa till en pydantic modell
+# https://pokeapi.co/api/v2/
+
+def get_pokemon(pokemon_name: str):
+    """
+    Funktion för att hämta och hitta rätt pokemon.
+    Använder mig av objekt i exercise_schema med den datan jag vill få ut
+    """
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}"
+    print(f"\n Response from: {url} \n")
+    with httpx.Client() as client:
+        response = client.get(url)
+        response.raise_for_status()
+        data = response.json()
+        pokemon_obj = PokemonSchema(**data) # **data == dictionary unpacking
+        return pokemon_obj
+    
+if __name__ == "__main__":
+    try: 
+        my_team = ["pikachu", "gengar", "snorlax", "mewtwo", "mew"]
+        for i, pokemon_name in enumerate(my_team):
+            team = get_pokemon(pokemon_name)
+            print(f"{team}")
+
+    except Exception as e:
+        print(f"Någonting gick fel, felkod: {e}")
---
+++ exercises/exersice_schema/ex_03_pokemon_schema.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+# Schema för exercise 03, pokemon GET.
+# 1) Bestäm vad du vill spara ifrån API't.
+# API't ger tillbaka massor av data(json) men jag vill enbart ha viss data.
+
+# Skapa en klass med de parametrar jag vill ha.
+
+class PokemonSchema(BaseModel):
+    name: str
+    base_experience: int
+    id: int
+    height: int
+    weight: int 
+    order: int
